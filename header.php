bab159842d45b11f9dbb74129539df8c781c5a9e
jchardin <jerome.chardin@outook.com>
2019-10-19 11:44:46 +0200

    header link

--- header.php
+++ header.php
@@ -1,8 +1,10 @@
 <?php
-
-
 $html = "<p>Je suis le header</p>";
 echo $html;
-
-
 ?>
+
+
+<ul>
+	<li><a htref="inscription.php">Inscription</a></li>
+	<li><a htref="panier.php">Panier</a></li>
+</ul>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 	<head meta charset="utf-8" />
 	<title>Ici On vend des cereales !</title>
+	<link rel="stylesheet" type="text/css" href="header.css">
 	</head>
 	<body>
 		<p>Hello je suis la page principal</p>
